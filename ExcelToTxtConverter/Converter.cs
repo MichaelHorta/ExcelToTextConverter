4a94e260e35e659f0538fb28c0d8435a8e7dae21
Michael Horta Fleitas <michaelhorta32@gmail.com>
2020-09-02 09:49:28 -0400

    update reading excel using ExcelReaderFactory

--- ExcelToTxtConverter/Converter.cs
+++ ExcelToTxtConverter/Converter.cs
@@ -1,8 +1,8 @@
-﻿using OfficeOpenXml;
+﻿using ExcelDataReader;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
-using System.Reflection;
 using System.Text;
 using System.Xml.Linq;
 
@@ -18,38 +18,38 @@ namespace ExcelToTxtConverter
             Definition = definition;
         }
 
-        public IDictionary<string, StringBuilder> Execute(byte[] excelData, Func<int, IList<ColumnHeadData>, ExcelWorksheet, string> grouperFunction = null)
+        public IDictionary<string, StringBuilder> Execute(byte[] excelData, Func<int, IList<ColumnHeadData>, DataTable, string> grouperFunction = null, Func<int, IList<ColumnHeadData>, DataTable, bool> ignoreRowFunction = null)
         {
             try
-            {
-                var lceExcelDataStream = new MemoryStream(excelData);
-                var package = new ExcelPackage();
-                package.Load(lceExcelDataStream);
-                ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+            {   
+                var tempFilenamePath = Path.GetTempFileName();
+                File.WriteAllBytes(tempFilenamePath, excelData);
+                System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                using (var stream = File.Open(tempFilenamePath, FileMode.Open, FileAccess.Read))
+                {
+                    // Auto-detect format, supports:
+                    //  - Binary Excel files (2.0-2003 format; *.xls)
+                    //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
+                    using (var reader = ExcelReaderFactory.CreateReader(stream))
+                    {
+                        var result = reader.AsDataSet();
+                        DataTable dataTable = result.Tables[0];
 
-                if (null == grouperFunction)
-                    grouperFunction = new Func<int, IList<ColumnHeadData>, ExcelWorksheet, string>(DefaultGrouperFunction);
+                        var lceTxtWriter = new TextWriterBase(dataTable, Definition);
+                        lceTxtWriter.Execute(grouperFunction, ignoreRowFunction);
+                        ColumnList = lceTxtWriter.ColumnList;
 
-                var lceTxtWriter = new TextWriterBase(worksheet, Definition);
-                lceTxtWriter.Execute(grouperFunction);
-                ColumnList = lceTxtWriter.ColumnList;
+                        reader.Close();
 
-                return lceTxtWriter.GetBuilders();
+                        return lceTxtWriter.GetBuilders();
+                    }
+                }
+                
             }
             catch (Exception ex)
             {
                 throw new Exception("Error executing conversion", ex);
             }
-        }
-
-        static string defaultSeparatorGuid = null;
-        private static string DefaultGrouperFunction(int indexRecord, IList<ColumnHeadData> lceColumnList, ExcelWorksheet excelWorksheet)
-        {
-            if (string.IsNullOrEmpty(defaultSeparatorGuid))
-            {
-                defaultSeparatorGuid = Guid.NewGuid().ToString();
-            }
-            return defaultSeparatorGuid;
         }
     }
 }
